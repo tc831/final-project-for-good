234c38a89d01e55e7a60b9d13299b426b5a495a0
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-07-25 22:40:50 +1000

    machine learning

---
+++ final_project/app_old.py
@@ -0,0 +1,18 @@
+from tokenize import String
+from flask import Flask, jsonify, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+import pandas as pd
+import os
+
+
+# engine = create_engine(url)
+app = Flask(__name__)
+app = Flask(__name__, static_folder="templates")
+
+
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
