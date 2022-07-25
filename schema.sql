18dbf71b316c9ec6b756c6956a453575f07e28f2
hiu-lai <97108762+hiu-lai@users.noreply.github.com>
2022-07-25 21:53:45 +1000

    postgres connection removal

---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS matches_final cascade;
+
+CREATE TABLE matches_final (
+"date" DATE, 
+"time" VARCHAR, 
+"comp" VARCHAR,  
+"round" VARCHAR, 
+"day" VARCHAR, 
+"venue" VARCHAR,  
+"result" VARCHAR,  
+"gf" INT, 
+"ga" INT, 
+"opponent"  VARCHAR,   
+"xg" FLOAT, 
+"xga" FLOAT, 
+"poss" INT, 
+"attendance" INT, 
+"captain" VARCHAR,
+"formation" VARCHAR, 
+"referee" VARCHAR, 
+"match_report" VARCHAR, 
+"sh" INT, 
+"sot" INT, 
+"dist" FLOAT,
+"fk" INT, 
+"pk" INT, 
+"pkatt" INT, 
+"season" INT, 
+"team" VARCHAR
+);
